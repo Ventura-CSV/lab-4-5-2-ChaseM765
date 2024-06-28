9bcd91493f53f0d8baa659ff10073b938a8ae87d
ChaseM765 <ChaseM765>
2024-06-28 00:32:50 -0700

    while loop added

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    while total < 100:
+        ran = random.randint(0,100)
+        numbers.append(ran)
+        total += ran
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
